e39c96c5cfb5ebc16730414eefb5c5b29b654a06
akaibot <mantlover@gmail.com>
2018-04-07 21:52:48 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,564 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', message => {
+  if (message.content === 'هلا') {
+    message.reply('اهلين؟');
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'شيباكو') {
+    message.reply('اهلين؟');
+  }
+});
+bot.on('message', message => {
+  if (message.content === 'الو') {
+    message.reply('هاه');
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'ولد') {
+    message.reply('انا ابوك');
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'تتابع انمي') {
+    message.reply('يب');
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'باك') {
+        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;        message.reply('بااااااااك');
+;
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === '-ب2') {
+    message.reply('بروح وبجي')
+        message.reply('بروح وبجي');
+;
+                                                                message.reply('بروح وبجي');
+                                                                    message.reply('بروح وبجي');
+                                                                        message.reply('بروح وبجي');
+                                                                            message.reply('بروح وبجي');    message.reply('بروح وبجي');
+                                                                                message.reply('بروح وبجي');
+                                                                                ;
+  }
+});
+bot.on('message', message => {
+  if (message.content === '-ش') {
+    message.reply('كروز بيروح شوي وبيرجع');
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'تخلي') {
+    message.reply('تعرف تمص');
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'ريلاكس') {
+    message.reply('ريلاكس المز');
+        message.reply('امزح')
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'كفو') {
+    message.reply('تقصدني');
+        message.reply('اكيد تقصدني')
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'xnxx') {
+    message.reply('ماضيك');
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'اسكت') {
+    message.reply('مابسكت');
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'pornhub') {
+    message.reply('...ز');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'كروز') {
+    message.reply('عمك');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'cruz') {
+    message.reply('عمك');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'تخسي') {
+    message.reply('اقول لايكثر');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'relax') {
+    message.reply('عمك');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'relaxs') {
+    message.reply('عمك');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'ليفاي') {
+    message.reply('المزة');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'انا') {
+    message.reply('الحب');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'بروب') {
+    message.reply('تيت');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'اشتري') {
+    message.reply('طيب والمطلوب');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'xlr') {
+    message.reply('عمك');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'اكسلر') {
+    message.reply('عمك');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'خلدلد') {
+    message.reply('عمك');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'من انت') {
+    message.reply('عمك');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'iKlz.VFX') {
+    message.reply('عمك');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'iKlz') {
+    message.reply('عمك');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'Klz') {
+    message.reply('عمك');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'كلز') {
+    message.reply('عمك');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'طارق') {
+    message.reply('عمك');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'يابوي') {
+    message.reply('هلا');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'بوت') {
+    message.reply('هلا');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'bot') {
+    message.reply('هلا');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'زق') {
+    message.reply('انت');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'كس') {
+    message.reply('ك');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'كلب') {
+    message.reply('انت');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'كس') {
+    message.reply('انت');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'فين') {
+    message.reply('طيزك');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'طيزك حمرا') {
+    message.reply('انت');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'تيزك') {
+    message.reply('انت');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'طيزك') {
+    message.reply('انت');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'كس امك') {
+    message.reply('انت');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'خنيث') {
+    message.reply('انت');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'يخنيث') {
+    message.reply('انت');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'قحبة') {
+    message.reply('انت');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'قحبه') {
+    message.reply('انت');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'ياقحبة') {
+    message.reply('انت');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'على زبي') {
+    message.reply('على زبي الالكتروني');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'ياعرص') {
+    message.reply('انت');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'منيوك') {
+    message.reply('انت');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'ياشرموطة') {
+    message.reply('انت');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'ياقواد') {
+    message.reply('انت');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'بنت') {
+    message.reply('انت');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'كلزق') {
+    message.reply('انت');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'كل زق') {
+    message.reply('انت');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'هاروهيرو') {
+    message.reply('سحبة');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'السلام') {
+    message.reply('وعليكم السلام ورحمة الله وبركاته');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'سلام') {
+    message.reply('وعليكم السلام ورحنة الله وبركاته');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'join') {
+    message.reply('وعليكم السلام ورحنة الله وبركاته');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'لعن') {
+    message.reply('لاتلعن');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'شيباكو') {
+    message.reply('عمانك');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'الشيباكو') {
+    message.reply('عمانك');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'chibaku') {
+    message.reply('عمانك');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'يوتيوب') {
+    message.reply('روح شوف قناة الشيباكو');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'youtube') {
+    message.reply('روح شوف قناة الشيباكو');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'اغنية') {
+    message.reply('روح شوف قناة الشيباكو');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'يتيوب') {
+    message.reply('روح شوف قناة الشيباكو');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'مقطع') {
+    message.reply('روح شوف قناة الشيباكو');
+
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === 'مقطع') {
+    message.reply('المرة هذه مامزح');
+
+  }
+});
+
+bot.on('message', message => {
+         if(message.author.bot) return;
+  if (message.content === 'ق1') {
+    message.reply('يمنع');
+
+  }
+});
+
+bot.login('MzY2MjYzNDUxODAwMTc0NjAy.DLqVCg.8jSom0TVdHwx83m4oqGFfuQqcE0');
